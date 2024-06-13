365746be070097a74a0e6d4973c125e4a6510a6e
Abdul Faqih <abdulfaqih7777@gmail.com>
2024-06-13 15:12:32 +0700

    fix session

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 
 const generateToken = (id, role) => jwt.sign({ id, role }, process.env.JWT_SECRET, {
-  expiresIn: '1h',
+  expiresIn: '30000d',
 });
 
 export default generateToken;
